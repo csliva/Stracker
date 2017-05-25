07c9c90e9dfd84a0a7565fb75e3f96989e949626
csliva <colt.sliva@gmail.com>
2017-05-25 13:35:48 -0400

    Update Seed Process
    Testing for larger user counts

--- api/priv/repo/seeds.exs
+++ api/priv/repo/seeds.exs
@@ -12,12 +12,15 @@
 defmodule Stracker.DatabaseSeeder do
   alias Stracker.Repo
   alias Stracker.Post
+  alias Stracker.User
 
-  def insert_post do
+  def insert_post(count) do
+    IO.puts(count)
     title = Elixilorem.words 2
     random_number = :rand.uniform(500)
     content = Elixilorem.words 5
-    random_user = :rand.uniform(1)
+    #random_user = :rand.uniform(5)
+    random_user = count
     Repo.insert! %Post{
       post_title: title,
       time: random_number,
@@ -26,10 +29,23 @@ defmodule Stracker.DatabaseSeeder do
     }
   end
 
+  def insert_user(count) do
+    changeset = Stracker.User.registration_changeset(%Stracker.User{}, %{
+      username: "demo#{count}",
+      email: "demo#{count}@gmail.com",
+      password: "demo123"
+    })
+    Stracker.Repo.insert(changeset)
+  end
+
+  def add_100_posts(count) do
+    (1..100) |> Enum.each(fn _ -> Stracker.DatabaseSeeder.insert_post(count) end)
+  end
+
   def clear do
     Repo.delete_all
   end
 end
 
-(1..100) |> Enum.each(fn _ -> Stracker.DatabaseSeeder.insert_post end)
+(1..1000) |> Enum.each(fn(count) -> Stracker.DatabaseSeeder.add_100_posts(count) end)
 #fn _ -> Stracker.DatabaseSeeder.clear end
