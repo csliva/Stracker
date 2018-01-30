73c4a97bd2febebd25be06c06d3337aeada3500d
csliva <colt.sliva@gmail.com>
2018-01-30 10:10:22 -0500

    events api returns all events

--- api/web/controllers/api/event_controller.ex
+++ api/web/controllers/api/event_controller.ex
@@ -28,11 +28,9 @@ defmodule Stracker.EventController do
             case Repo.update(changeset) do
               # if it works render the result, if not throw api error
               {:ok, event} ->
-                render(conn, "show.json", event: event)
+                render_events_success(conn, user_id, task_id)
               {:error, changeset} ->
-                conn
-                |> put_status(:unprocessable_entity)
-                |> render(Stracker.ChangesetView, "error.json", changeset: changeset)
+                add_event_error(conn, changeset)
             end
           # default action, previous event exists with filled in end_time
           true ->
@@ -43,7 +41,10 @@ defmodule Stracker.EventController do
                 "user_id" => user_id,
                 "task_id" => task_id
               })
-            new_row(conn, changeset)
+              case Repo.insert(changeset) do
+                {:ok, _} -> render_events_success(conn, user_id, task_id)
+                {:error} -> add_event_error(conn, changeset)
+              end
         end
       :error ->
         # Nothing exists yet. We need to create the first row
@@ -53,22 +54,27 @@ defmodule Stracker.EventController do
             "user_id" => user_id,
             "task_id" => task_id
           })
-        new_row(conn, changeset)
+          case Repo.insert(changeset) do
+            {:ok, _} -> render_events_success(conn, user_id, task_id)
+            {:error} -> add_event_error(conn, changeset)
+          end
     end
   end
 
-  defp new_row(conn, changeset) do
-    case Repo.insert(changeset) do
-      {:ok, event} ->
-        conn
-        |> put_status(:created)
-        |> put_resp_header("location", event_path(conn, :show, event))
-        |> render("show.json", event: event)
-      {:error} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> render(Stracker.ChangesetView, "error.json", changeset: changeset)
-    end
+  defp render_events_success(conn, user_id, task_id) do
+    events = Repo.all(
+      from e in Event,
+      select: e,
+      where: ^task_id == e.task_id and ^user_id == e.user_id,
+      order_by: [desc: e.updated_at]
+    )
+    render(conn, "index.json", events: events)
+  end
+
+  defp add_event_error(conn, changeset) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(Stracker.ChangesetView, "error.json", changeset: changeset)
   end
 
   def get_by_task(conn, %{"user_id" => user_id, "task_id" => task_id}) do
