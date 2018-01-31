7d02ed8660abba6f9c30ea24c16cc0e96f69a9cd
treptow <treptow@apollointeractive.com>
2018-01-30 22:53:04 -0800

    working on front-end

--- web/src/components/Navbar/index.js
+++ web/src/components/Navbar/index.js
@@ -28,22 +28,18 @@ class Navbar extends Component {
             <li className="nav__item">
               <Link className="nav__link nav__link--branding" to="/">&int;tracker</Link>
             </li>
-            {isAuthenticated &&
-                <li className="nav__item">
-                  <button
-                    className="nav__button"
-                    type="button"
-                    onClick={this.handleLogout}>
-                     Logout (<span>{currentUser.username}</span>)
-                  </button>
-                </li>
-            }
             <li className="nav__item nav__item--right">
               <Link className="nav__link"  to="/boards">
                 Go to Boards
               </Link>
             </li>
-
+            {isAuthenticated &&
+              <li className="nav__item">
+                <button className="nav__button" type="button" onClick={this.handleLogout}>
+                  Logout (<span>{currentUser.username}</span>)
+                </button>
+              </li>
+            }
           </ul>
         </nav>
 
